823b202bf094aa40e29b1be4632af6c12a867556
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-03-06 19:50:02 -0300

    Dando continuidade ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './app.css'
+import iconLobo from '../src/assets/lobo.png';
+import Menu from './components/menu/Menu'
+import { GiConfirmed } from "react-icons/gi";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <Menu />
+      <main>
+        <img src={iconLobo} alt="lobo" className='iconLobo' />
+
+        <section className='painelValorNutricinal'>
+          <div className='bolasValores'>
+            <p>0g</p>
+            <p className='descricaoNutricional'>Carbo</p>
+          </div>
+          <div className='bolasValores'>
+            <p>0g</p>
+            <p className='descricaoNutricional'>Carbo</p>
+          </div>
+          <div className='bolasValores'>
+            <p>0g</p>
+            <p className='descricaoNutricional'>Carbo</p>
+
+          </div>
+        </section>
+
+        <h1>Customize Your Dish</h1>
+
+        <section className='painelComidas'>
+          <form action="">
+            <div>
+              <label htmlFor="campoFood">Food</label>
+              <input type="text" name="" id=""  required />
+            </div>
+            <div>
+              <label htmlFor="campoVolume">Volume</label>
+              <input type="text" name="" id=""  required />
+            </div>
+            <button > <GiConfirmed/> </button>
+
+
+          </form>
+          <button></button>
+
+        </section>
+
+      </main>
+
+    </div>
   )
 }
 
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,10 @@
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import './Menu.css'
+
+export default function Menu (){
+    return(
+        <div>
+          <MdOutlineRestaurantMenu className="menuIcon" />
+        </div>
+    )
+}
