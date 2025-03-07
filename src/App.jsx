a56c6092e59f66939b60a98f1978097186129014
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-03-07 12:58:39 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import iconLobo from '../src/assets/lobo.png';
 import Menu from './components/menu/Menu'
 import { FaRegCircleCheck } from "react-icons/fa6";
 import { SlReload } from "react-icons/sl";
+import { useState } from 'react';
+import alimentos from "./alimentos/alimentos.json";
 
 function App() {
+
+  const [food, setFood] = useState("");
+  const [volume, setVolume] = useState("");
+
+  const [carbo, setCarbo] = useState(0);
+  const [proteina, setProteina] = useState(0);
+  const [caloria, setCaloria] = useState(0);
+
+
+
+
   return (
     <div className='app'>
       <Menu />
       <main>
         <div className='caixaImg'>
-           <img src={iconLobo} alt="lobo" className='iconLobo' />
+          <img src={iconLobo} alt="lobo" className='iconLobo' />
         </div>
-       
+
 
         <section className='painelValorNutricinal'>
 
@@ -35,23 +48,49 @@ function App() {
 
         <h1>Customize Your Dish</h1>
 
+
         <section className='painelComidas'>
           <form className='fromularioComidaVolume' action="">
-            <div>
+            <div className='painelIpuntoFood'>
+              <div className='painelSugestao'>
+
+              </div>
               <label htmlFor="campoFood">Food</label>
-              <input type="text" name="" id="food"  required />
+              <input
+                id="food"
+                type="text"
+                onChange={(e) => {
+                  console.log(e.target.name);
+                }}
+                name="eu"
+              />
             </div>
-            <div>
+            <div className='painelIpuntVolume'>
               <label htmlFor="campoVolume">Volume</label>
-              <input type="text" name="" id="volume"  required />
+              <input type="text" name="" id="volume" required />
+
+              <select id="unidade" name="unidade">
+                <option value="g">g</option>
+                <option value="ml">ml</option>
+              </select>
+
             </div>
-            <button > <FaRegCircleCheck className='iconComfirm'/> </button>
+            <button > <FaRegCircleCheck className='iconComfirm' /> </button>
+
           </form>
-          <button></button>
+          <ul className='listaAlimentos'>
+            <li>alimento</li>
+            <li>alimento</li>
+            <li>alimento</li>
+            <li>alimento</li>
+            <li>alimento</li>
+            <li>alimento</li>
+          </ul>
+
 
         </section>
 
-        < SlReload className='iconReload'/>
+        < SlReload className='iconReload' />
 
       </main>
 
