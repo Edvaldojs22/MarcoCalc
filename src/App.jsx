c1e05ee831d02fec909316e0ff74ed332f095783
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-03-06 21:08:49 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import './app.css'
 import iconLobo from '../src/assets/lobo.png';
 import Menu from './components/menu/Menu'
-import { GiConfirmed } from "react-icons/gi";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import { SlReload } from "react-icons/sl";
 
 function App() {
   return (
     <div className='app'>
       <Menu />
       <main>
-        <img src={iconLobo} alt="lobo" className='iconLobo' />
+        <div className='caixaImg'>
+           <img src={iconLobo} alt="lobo" className='iconLobo' />
+        </div>
+       
 
         <section className='painelValorNutricinal'>
+
           <div className='bolasValores'>
             <p>0g</p>
             <p className='descricaoNutricional'>Carbo</p>
           </div>
+
           <div className='bolasValores'>
             <p>0g</p>
             <p className='descricaoNutricional'>Carbo</p>
           </div>
+
           <div className='bolasValores'>
             <p>0g</p>
             <p className='descricaoNutricional'>Carbo</p>
@@ -29,22 +36,22 @@ function App() {
         <h1>Customize Your Dish</h1>
 
         <section className='painelComidas'>
-          <form action="">
+          <form className='fromularioComidaVolume' action="">
             <div>
               <label htmlFor="campoFood">Food</label>
-              <input type="text" name="" id=""  required />
+              <input type="text" name="" id="food"  required />
             </div>
             <div>
               <label htmlFor="campoVolume">Volume</label>
-              <input type="text" name="" id=""  required />
+              <input type="text" name="" id="volume"  required />
             </div>
-            <button > <GiConfirmed/> </button>
-
-
+            <button > <FaRegCircleCheck className='iconComfirm'/> </button>
           </form>
           <button></button>
 
         </section>
+
+        < SlReload className='iconReload'/>
 
       </main>
 
