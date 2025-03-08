e593e220e3a9041ee74bf2ecc5758d3ccdc0739a
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-03-08 10:36:11 -0300

    Att

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
 import './app.css'
 import iconLobo from '../src/assets/lobo.png';
 import Menu from './components/menu/Menu'
-import { FaRegCircleCheck } from "react-icons/fa6";
 import { SlReload } from "react-icons/sl";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import alimentos from "./alimentos/alimentos.json";
+import { FaCheck } from "react-icons/fa";
+
 
 function App() {
-
-  const [food, setFood] = useState("");
-  const [volume, setVolume] = useState("");
 
   const [carbo, setCarbo] = useState(0);
   const [proteina, setProteina] = useState(0);
   const [caloria, setCaloria] = useState(0);
+
+  const [quantidade, setQuantidade] = useState(1);
+
+
+  const [listaAlimentos, setListaAlimento] = useState([]);
+
+  const [alimento, setAlimento] = useState("");
+
+
+  //Codigo responsavel por filtra o alimento do input e colocar na lista alimentos selecionados
+  const buscaAlimento = (event) => {
+    event.preventDefault();
+
+    if(!alimento.trim()){
+      alert("Por favor, preencha todos os campos antes de adicionar.");
+      return;
+    }
+
+    const filtrado = alimentos.filter((item) => {
+      return item.nome.toLocaleLowerCase().includes(alimento.toLocaleLowerCase())
+    });
+
+    if (filtrado.length == 0) {
+      console.log("Alimento não encontrado");
+      return;
+    } else {
+      {
+        setListaAlimento((prevLista) => [...prevLista, ...filtrado]);
+
+      }
+    }
+ 
+  }
+
+  // Responsavel por fazer a somas dos nutrientes dos alimentos
+  useEffect(() => {
+    const totalCarbo = listaAlimentos.reduce((acc, item) => acc + item.carboidrato, 0);
+    const totalProteina = listaAlimentos.reduce((acc, item) => acc + item.proteina, 0);
+    const totalCaloria = listaAlimentos.reduce((acc, item) => acc + item.calorias, 0);
+
+
+    setCarbo(totalCarbo);
+    setProteina(totalProteina);
+    setCaloria(totalCaloria);
+  }, [listaAlimentos]);
 
 
 
@@ -27,21 +70,23 @@ function App() {
         </div>
 
 
+
+
         <section className='painelValorNutricinal'>
 
           <div className='bolasValores'>
-            <p>0g</p>
+            <p>{(carbo * quantidade).toFixed(2)}g</p>
             <p className='descricaoNutricional'>Carbo</p>
           </div>
 
           <div className='bolasValores'>
-            <p>0g</p>
-            <p className='descricaoNutricional'>Carbo</p>
+            <p>{(caloria * quantidade).toFixed(2)}g</p>
+            <p className='descricaoNutricional'>Kcal</p>
           </div>
 
           <div className='bolasValores'>
-            <p>0g</p>
-            <p className='descricaoNutricional'>Carbo</p>
+            <p>{(proteina * quantidade).toFixed(2)}g</p>
+            <p className='descricaoNutricional'>Protein</p>
 
           </div>
         </section>
@@ -55,19 +100,25 @@ function App() {
               <div className='painelSugestao'>
 
               </div>
-              <label htmlFor="campoFood">Food</label>
+              <label htmlFor="campoFood">Comida</label>
               <input
                 id="food"
                 type="text"
-                onChange={(e) => {
-                  console.log(e.target.name);
-                }}
-                name="eu"
-              />
+                value={alimento}
+                onChange={(e) => setAlimento(e.target.value)}
+                required />
             </div>
             <div className='painelIpuntVolume'>
-              <label htmlFor="campoVolume">Volume</label>
-              <input type="text" name="" id="volume" required />
+              <label htmlFor="campoVolume">medida</label>
+              <input
+                type="text"
+                name=""
+                id="volume"
+                value={quantidade}
+                onChange={(e) => {
+                  setQuantidade(e.target.value);
+                }}
+                required />
 
               <select id="unidade" name="unidade">
                 <option value="g">g</option>
@@ -75,20 +126,19 @@ function App() {
               </select>
 
             </div>
-            <button > <FaRegCircleCheck className='iconComfirm' /> </button>
+            <button onClick={buscaAlimento} className='botaoAdicionar'>Adicionar</button>
 
           </form>
-          <ul className='listaAlimentos'>
-            <li>alimento</li>
-            <li>alimento</li>
-            <li>alimento</li>
-            <li>alimento</li>
-            <li>alimento</li>
-            <li>alimento</li>
-          </ul>
+           <ul className='listaAlimentos'>
+            {listaAlimentos.map((a, index) =>(
+              <li key={index}>{a.nome} {quantidade}g<FaCheck className='iconCheck'/></li>
+            ))}
+
+          </ul> 
 
 
         </section>
+
 
         < SlReload className='iconReload' />
 
